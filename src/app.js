d81c469ce848a4a869528828002bf5db2b4a3e61
Jumpvzl <40275118+Jumpvzl@users.noreply.github.com>
2024-08-31 02:51:22 +0000

    se termina el codigo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+/*
+thegreatjogger.com =  [0][0][0]
+thegreatracoon.com =  [0][0][1]
+ourgreatjogger.com =  [1][0][0]
+ourgreatracoon.com =  [1][0][1]
+thebigjogger.com   =  [0][1][0]
+thebigracoon.com   =  [0][1][0]
+ourbigjogger.com   =  [1][1][0]
+ourbigracoon.com   =  [1][1][1]
+*/
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cadena = " ";
+  for (let i = 0; i <= pronoun.length - 1; i++) {
+    for (let a = 0; a <= adj.length - 1; a++) {
+      for (let b = 0; b <= noun.length - 1; b++) {
+        cadena += pronoun[i] + adj[a] + noun[b] + ".com<br>";
+      }
+    }
+  }
+  const list = document.getElementById("list");
+  list.innerHTML = '<li class="list-group-item">' + cadena + "</li>";
 };
